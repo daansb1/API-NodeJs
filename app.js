f2081a187fc56e5cb04527e086ff2b4fe3f7d940
daansb1 <32489561+daansb1@users.noreply.github.com>
2020-01-26 17:24:34 -0300

    Add files via upload
    Arquivos API

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const router = express.Router();
+
+// Carrega as Rotas
+const indexRoute =  require('./routes/index-route');
+const productRoute =  require('./routes/product-route');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+
+module.exports = app;
